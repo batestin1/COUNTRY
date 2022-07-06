bb6e554d02c386b3719a53cde5498f054c98ce6b
batestin1 <bates@dell.localdomain>
2022-07-06 15:09:28 -0300

    v1 - 20220706

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center">
+<img src="https://img.shields.io/static/v1?label=SHELL%20POR&message=MAYCON%20BATESTIN&color=7159c1&style=flat-square&logo=ghost"/>
+
+
+<h3> <p align="center">SHELL - COUNTRY </p> </h3>
+<h3> <p align="center"> ================= </p> </h3>
+
+>> <h3> Step I - HOW WORKS </h3>
+
+<p> After downloading clone the repository run the file country.sh </p>
+
+<p> It asks you to enter the name of some country. And returns a capital. This answer is by querying wikipedia  </p>
+
+>> <h3> Help command </h3>
+<p> type: </p>
+
+`sh country.sh --ajuda`
+<p> out </p>
+
+```
+#========================= COMANDO DE AJUDA ============================================================================#
+# * Este programa gere um arquivo compactado de backup de todo o diretório home do usuário atual.                       #
+# * Em caso de um backup já existir, ele automaticamente lhe pergunta se deseja sobreescrever ou cancelar o backup.     #
+# * Os backups são realizados automaticamente a cada 7 dias                                                             #
+# * Os arquivos de backup são de extensão 'tgz' e podem ser descompactados usando o tar                                 #
+========================================================================================================================#
+```
---
+++ country.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#####################################################################
+#
+# script name: country
+# created in: 06/29/22
+# modified in: 14:13:28
+#
+# summary: Retorna a capital de um pais
+#                                               developed by: bates
+#####################################################################
+
+clear
+#variables
+
+PAM=$1
+
+
+if [ $PAM = "--ajuda" ] 2>/dev/null; then
+    echo "#======================================================================================#"
+    echo "# O Programa consulta a api do site wikipedia.org, em versão PT-BR                     #"
+    echo "# O primeiro input que ele necessita é o nome do país.                                 #"
+    echo "#======================================================================================#"
+    echo ""
+    echo "#======================================================================================#"
+    echo "# Para saber a versão do programa digite o comando abaixo                              #"
+    echo "# sh country.sh --version                                                              #"
+    echo "#======================================================================================#"
+fi
+
+if [ $PAM = "--version" ] 2>/dev/null; then
+    echo "#======================================================================================#"
+    echo "# Versão do Programa 1.0.0                                                             #"
+    echo "#======================================================================================#"
+    echo ""
+    echo "#======================================================================================#"
+    echo "# Para saber mais sobre o programa digite o comando                                    #"
+    echo "# sh country.sh --ajuda                                                                #"
+    echo "#======================================================================================#"
+fi
+
+clear
+
+if [ $(uname) = "Linux" ]; then
+
+    sudo apt-get update -y > /dev/null 2> /dev/null && sudo apt-get upgrade -y > /dev/null 2> /dev/null
+    sudo apt-get install curl  > /dev/null 2> /dev/null
+
+    sudo apt-get install jq > /dev/null 2> /dev/null
+
+    #game
+elif [ $(uname) = "Darwin" ]; then
+    sudo apt-get update -y > /dev/null 2> /dev/null && sudo apt-get upgrade -y > /dev/null 2> /dev/null
+    /usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install) > /dev/null 2> /dev/null && sudo apt-get upgrade -y > /dev/null 2> /dev/null
+    brew install jq > /dev/null 2> /dev/null && sudo apt-get upgrade -y > /dev/null 2> /dev/null
+    brew install coreutil > /dev/null 2> /dev/null && sudo apt-get upgrade -y > /dev/null 2> /dev/null
+    #game
+fi
+
+
+if [ country.sh ] 2>/dev/null; then
+
+    read -p "Insira o nome do pais: " NAME
+    NAME_1=$(echo $NAME | sed -e "s/\b\(.\)/\u\1/g")
+    NAME_COUNTRY=$(echo $NAME_1 | sed 's/ /_/g')
+
+    RES=$(curl -s -L https://pt.wikipedia.org/wiki/$NAME_COUNTRY | grep -i "vertical-align:top" | sed "1, 5 d"  | sed "s/<[^>]*>/\n/g" | sed "1,2 d" | head -n1 | sed "s/\n//g")
+    RES_P=$(echo $RES | sed -e "s/\b\(.\)/\u\1/g")
+
+
+    if [ "$RES_P" = "Cidade Mais Populosa" ]; then
+        RES=$(curl -s -L https://pt.wikipedia.org/wiki/$NAME_COUNTRY  | grep -i "vertical-align:top" | sed "1, 5 d"  | sed "s/<[^>]*>/\n/g" | sed "1,5 d" | head -n1)
+        RES_P=$(echo $RES | sed -e "s/\b\(.\)/\u\1/g")
+    fi
+
+    if [ "$RES_P" = "" ]; then
+        RES_P="Não conseguimos encontrar essa resposta!"
+    fi
+
+
+    echo "PAIS: $NAME_1"
+    echo "CAPITAL: $RES_P"
+
+    read -p "Deseja pesquisar novamente? (Y/n) " OPT
+
+    if [ "$OPT" = "Y" -o "$OPT" = "y" -o "$OPT" = "" ]; then
+        sh country.sh
+    else
+        echo "Obrigado!"
+    fi
+fi
